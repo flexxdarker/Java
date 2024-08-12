a78cdb53162d5319adf060dd834ae22b6cecb056
Max <125499561+flexxdarker@users.noreply.github.com>
2024-08-13 00:35:51 +0300

    clear code.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,26 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        int n;
-//        Scanner cin = new Scanner(System.in);
-//        System.out.print("Enter n: ");
-//        n = cin.nextInt();
-//        System.out.println("n->" + n);
-//        int [] arr = new int[n];
-//        for(int i = 0; i<n; i++) {
-//            arr[i] = rancdomValue(1, 60);
-//        }
-//        for(int item: arr){
-//            System.out.printf("%d\t",item);
-//        }
-//        System.out.println();
-//        int count = 0;
-//        int i = 0;
-//        while (i<n){
-//            if(arr[i]>20)
-//                count++;
-//            i++;
-//        System.out.println("older then 20:"+count);
+
         int i = 0;
         int score = 0;
             while (i < 3) {
@@ -43,8 +24,3 @@ public class Main {
         System.out.println("Your result: " + score);
         }
     }
-//    public static int rancdomValue(int min, int max)
-//    {
-//        Random rand = new Random();
-//        return rand.nextInt((max-min)+1)+min;
-//    }
